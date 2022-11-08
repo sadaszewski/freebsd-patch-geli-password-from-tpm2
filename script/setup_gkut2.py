31b24aedb008943b58e4210c1c4df4beac6c00cc
Stanislaw Adaszewski <s.adaszewski@adared.ch>
2022-11-08 20:52:04 +0100

    Add setup_gkut2.py.

---
+++ script/setup_gkut2.py
@@ -0,0 +1,133 @@
+
+import subprocess
+import os
+import random
+import tempfile
+import hashlib
+import base64
+import shutil
+from argparse import ArgumentParser
+
+
+def create_parser():
+    parser = ArgumentParser()
+    parser.add_argument('--efi-target-dir', type=str, default='/boot/efi/efi/freebsd/gkut2')
+    parser.add_argument('--geom', type=str)
+    parser.add_argument('--nbits', type=int, default=256)
+    parser.add_argument('--pcr-policy', type=str, default='sha256:0,2,4,7,8')
+    parser.add_argument('--marker-filename', type=str, default='/.passphrase_marker')
+    parser.add_argument('--iv-nbits', type=int, default=128)
+    parser.add_argument('--newpass', type=str)
+    parser.add_argument('--no-geli', action='store_true')
+    return parser
+
+
+def detect_geom():
+    geom = [ ln for ln in \
+        subprocess.check_output([ 'geli', 'list' ]).decode('utf-8').strip().split('\n') \
+            if 'Geom name:' in ln ][0]
+    geom = geom.split()[-1][:-len('.eli')]
+    return geom
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+
+    os.makedirs(args.efi_target_dir, exist_ok=True)
+
+    geom = args.geom or detect_geom()
+    print('ELI geom:', geom)
+
+    print('Generating new passphrase...')
+    newpass = args.newpass.encode('utf-8') or random.getrandbits(args.nbits).to_bytes(args.nbits // 8, 'little') 
+
+    print('Generating salt, TPM2 owner password, primary key auth value...')
+    newsalt, newownerpass, primaryauthvalue = [ random.getrandbits(args.nbits).to_bytes(args.nbits // 8, 'little') \
+        for _ in range(3) ]
+
+    print('Generating IV...')
+    iv = random.getrandbits(args.iv_nbits).to_bytes(args.iv_nbits // 8, 'little')
+
+    with open(os.path.join(args.efi_target_dir, 'salt'), 'wb') as f:
+        f.write(newsalt)
+
+    with open(os.path.join(args.efi_target_dir, 'iv'), 'wb') as f:
+        f.write(iv)
+
+    with tempfile.TemporaryDirectory() as d:
+        os.chmod(d, 0o700)
+
+        for fnam, data in { '.newpass': newpass, '.newownerpass': newownerpass,
+            '.primaryauthvalue': primaryauthvalue }.items():
+            with open(os.path.join(d, fnam), 'wb') as f:
+                f.write(data)
+
+        print('Clearing TPM2...')
+        subprocess.check_output([ 'tpm2_clear' ])
+
+        print('Taking ownership of TPM2...')
+        subprocess.check_output([ 'tpm2_changeauth',
+            '-c', 'owner', 'file:' + os.path.join(d, '.newownerpass') ])
+
+        print('Generating passphrase marker...')
+        sha256 = hashlib.sha256()
+        sha256.update(newsalt)
+        sha256.update(newpass)
+        new_passphrase_marker = sha256.hexdigest()
+        with open(args.marker_filename, 'w') as f:
+            f.write(new_passphrase_marker)
+        os.chmod(args.marker_filename, 0o600)
+
+        print('Reading PCRs...')
+        subprocess.check_output([ 'tpm2_pcrread',
+            args.pcr_policy,
+            '-o', os.path.join(d, '.pcrvalues') ])
+
+        print('Adjusting PCRs...')
+        st = os.stat(os.path.join(d, '.pcrvalues'))
+        subprocess.check_output([ 'truncate',
+            '-s', str(st.st_size - 32),
+            os.path.join(d, '.pcrvalues') ])
+
+        subprocess.check_output([ 'truncate',
+            '-s', str(st.st_size),
+            os.path.join(d, '.pcrvalues') ])
+
+        print('Generating policy digest...')
+        subprocess.check_output([ 'tpm2_createpolicy',
+            '-L', os.path.join(args.efi_target_dir, 'policydigest'),
+            '--policy-pcr',
+            '-l', args.pcr_policy,
+            '-f', os.path.join(d, '.pcrvalues') ])
+
+        print('Creating primary key...')
+        subprocess.check_output([ 'tpm2_createprimary',
+            '-G', 'aes128cfb',
+            '-a', 'fixedparent|fixedtpm|sign|decrypt|sensitivedataorigin|userwithauth',
+            '-L', os.path.join(args.efi_target_dir, 'policydigest'),
+            '-c', os.path.join(d, '.primarycontext'),
+            '-P', 'file:' + os.path.join(d, '.newownerpass'),
+            '-p', 'file:' + os.path.join(d, '.primaryauthvalue') ])
+
+        print('Encrypting passphrase...')
+        subprocess.check_output([ 'tpm2_encryptdecrypt',
+            '-c', os.path.join(d, '.primarycontext'),
+            '-p', 'file:' + os.path.join(d, '.primaryauthvalue'),
+            '-t', os.path.join(args.efi_target_dir, 'iv'),
+            '-o', os.path.join(args.efi_target_dir, 'passphrase.enc'),
+            os.path.join(d, '.newpass') ]) 
+
+        if not args.no_geli:
+            print('Modifying GELI passphrase...')
+            for i in range(2):
+                subprocess.check_output([ 'geli', 'setkey',
+                    '-n', str(i),
+                    '-J', os.path.join(d, '.newpass'),
+                    geom ])
+
+    print('Success!')
+
+
+if __name__ == '__main__':
+    main()
